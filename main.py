081601e6522caf4e0b5a9c2c7ee818d043bac89b
charliepremo <115026135+charliepremo@users.noreply.github.com>
2022-11-17 12:34:53 -0800

    added main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+'''
+
+Get to the end of the level while avoding the enemies
+
+'''
+
+# sources:
+
+# import libraries
+import pygame as pg
+from pygame.sprite import Sprite
+import random
+from random import randint
+
+# built in
+
+# installed modules or libraries
+
+# created modules or libraries
+
+# global variables
+
+# utility variables
